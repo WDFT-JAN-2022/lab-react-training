c9d73bd843a2370e1dcedc27e9ef422c8f8a1a18
Eduardo Beyra <eduardobeyra@hotmail.com>
2022-02-22 17:03:55 -0500

    iteration 2 eddie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import IdCard from './component/IdCard';
+import Greetings from './component/Greetings';
 
 function App() {
   return (
@@ -13,6 +14,10 @@ function App() {
         birth={new Date('1992-07-14')}
         picture="https://randomuser.me/api/portraits/men/44.jpg"
       />
+      <Greetings lang="de">Ludwig</Greetings>
+      <Greetings lang="fr">François</Greetings>
+      <Greetings lang="en">Eddie</Greetings>
+      <Greetings lang="es">Eduardo</Greetings>
     </div>
   );
 }
